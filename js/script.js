4d6d2ccd18282c5888448983a092866ff8e031ad
Alfredodaniello <alfredo_daniello@libero.it>
2024-03-30 21:22:23 +0100

    feat: add class active dinamic

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*
 Descrizione:
 Partendo dal markup fornito nello starter pack, rifare lo slider ma questa volta usando Vue.
-Bonus:
+
 Bonus:
 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
